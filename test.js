8065c202ed5b26e5dd8c39b5219394c784faec57
olegshprengelsnyk <oleg.shprengel@snyk.io>
2022-10-11 08:29:18 -0600

    init2

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+// deepcode ignore HardcodedNonCryptoSecret/test: <please specify a reason of ignoring this>
 var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
 test('renders learn react link', () => {
     render(<App />);
