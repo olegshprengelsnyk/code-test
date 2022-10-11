5ed0e41f27dcfbc14fd91db0a61943c6e66345f9
olegshprengelsnyk <oleg.shprengel@snyk.io>
2022-10-11 08:26:29 -0600

    init

---
+++ test.js
@@ -0,0 +1,6 @@
+var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
+test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
